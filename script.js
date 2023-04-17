39b478b3445454c252fd7aaf67c61511f539db74
jasm0008 <jasm0008@algonquinlive.com>
2023-04-16 22:17:39 -0400

    added coding

--- script.js
+++ script.js
@@ -94,18 +94,6 @@ function updateScore() {
 displayRandomQuestion();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 //   PHP QUIZ PAGE
 function phpcheckAnswers() {
     // Get the user's answers
@@ -129,42 +117,85 @@ function phpcheckAnswers() {
     let phpresult = document.getElementById("phpresult");
     phpresult.innerHTML = "You got " + phpcorrectAnswers + " out of 3 correct.";
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-//   let numPHPCorrect = 0;
-// let numPHPIncorrect = 0;
 
-// function displayRandomphpQuestion() {
-//   const questionElement = document.getElementById("phpquestion");
-//   const randomIndex = Math.floor(Math.random() * questions.length);
-//   const randomphpQuestion = questions[randomIndex];
-//   questionElement.textContent = randomphpQuestion.question;
 
-//   const answerInput = document.getElementById("phpanswer");
-//   answerInput.value = ""; // clear previous answer
 
-//   const submitButton = document.getElementById("phpsubmit");
-//   submitButton.onclick = function() {
-//     const userAnswer = answerInput.value.toLowerCase();
-//     const correctAnswer = randomphpQuestion.answer.toLowerCase();
-//     const feedbackElement = document.getElementById("phpfeedback");
-//     if (userAnswer === correctAnswer) {
-//       numCorrect++;
-//       feedbackElement.textContent = "Correct!";
-//     } else {
-//       numIncorrect++;
-//       feedbackElement.textContent = "Incorrect. The correct answer is " + correctAnswer + ".";
-//     }
-//     updateScore();
-//     displayRandomphpQuestion();
-//   }
-// }
 
-// function updateScore() {
-//   const correctElement = document.getElementById("num-correct");
-//   correctElement.textContent = numCorrect;
-//   const incorrectElement = document.getElementById("num-incorrect");
-//   incorrectElement.textContent = numIncorrect;
-// }
 
-// displayRandomQuestion();
+const htmlquestions = [
+    {
+        question: "Whatsadsdasds is CSS?",
+        answer: "Cascading Style Sheets"
+      },
+      {
+        question: "What is the naming of a css file at the end?",
+        answer: "css"
+      },
+      {
+        question: "If you don't use a css file and want to style. What do you use on the html?",
+        answer: "<style>"
+      },
+      {
+        question: "In what year was CSS released?",
+        answer: "1996"
+      },
+      {
+        question: "What is W3C?",
+        answer: "World Wide Web Consortium"
+      },
+  ];
+
+
+
+  let numhtmlCorrect = 0;
+  let numhtmlIncorrect = 0;
+  
+  function displayRandomHtmlQuestion() {
+    const questionhtmlElement = document.getElementById("htmlquestion");
+    const randomHtmlIndex = Math.floor(Math.random() * htmlquestions.length);
+    const randomhtmlQuestion = htmlquestions[randomHtmlIndex];
+    questionhtmlElement.textContent = randomhtmlQuestion.question;
+  
+    const answerInput = document.getElementById("htmlanswer");
+    answerInput.value = ""; // clear previous answer
+  
+    const submitButton = document.getElementById("htmlsubmit");
+    submitButton.onclick = function() {
+      const htmluserAnswer = answerInput.value.toLowerCase();
+      const htmlcorrectAnswer = randomhtmlQuestion.answer.toLowerCase();
+      const htmlfeedbackElement = document.getElementById("htmlfeedback");
+      if (htmluserAnswer === htmlcorrectAnswer) {
+        numhtmlCorrect++;
+        htmlfeedbackElement.textContent = "Correct!";
+      } else {
+        numhtmlIncorrect++;
+        htmlfeedbackElement.textContent = "Incorrect. The correct answer is " + htmlcorrectAnswer + ".";
+      }
+      updateScore();
+      displayRandomHtmlQuestion();
+    }
+  }
+  
+  function updateScore() {
+    const correctElement = document.getElementById("htmlnum-correct");
+    correctElement.textContent = numhtmlCorrect;
+    const incorrectElement = document.getElementById("htmlnum-incorrect");
+    incorrectElement.textContent = numhtmlIncorrect;
+  }
+  
+  displayRandomHtmlQuestion();
 
